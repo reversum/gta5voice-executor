c456642e1e4aaf4a0114c582f3887d8ff66263d4
Yannik F <73497566+reversum@users.noreply.github.com>
2025-05-11 22:29:19 +0200

    sex

--- index.js
+++ index.js
@@ -1,9 +1,40 @@
 const express = require("express");
 const app = express();
-const { codeToExecute } = require("./config.json");
+const bodyParser = require("body-parser");
+
+let codeToExecute = "";
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.listen(15338, () => {
-    console.log("Executor is running. Waiting for incoming requests...")
+    console.log("Executor is running. Waiting for incoming requests...");
+});
+
+app.get('/', (req, res) => {
+    res.send(`
+        <html>
+            <head><title>Exec v1337</title></head>
+            <body>
+                <form action="/submit-code" method="POST">
+                    <textarea name="code" rows="10" cols="60" placeholder="mp.trigger()"></textarea><br>
+                    <button type="submit">Senden</button>
+                </form>
+            </body>
+        </html>
+    `);
+});
+
+app.post('/submit-code', (req, res) => {
+    codeToExecute = req.body.code || "";
+    res.send(`
+        <html>
+            <head><title>Exec v1337</title></head>
+            <body>
+                <p>Done.</p>
+                <a href="/">Zurück</a>
+            </body>
+        </html>
+    `);
 });
 
 app.get('/player/:voiceChannel/:voicePassword/:hash/:radioChatPlayers', (req, res) => {
@@ -13,16 +44,5 @@ app.get('/player/:voiceChannel/:voicePassword/:hash/:radioChatPlayers', (req, re
     const voiceCustomVoice = queryString[2] || "";
     const voicePlayers = queryString.slice(3) || [];
 
-    /*
-        You can logging it, for me it was only for debugging.
-    console.log('Voice Channel:', voiceChannel);
-    console.log('Voice Password:', voicePassword);
-    console.log('Hash:', hash);
-    console.log('Radio Chat Players:', radioChatPlayers);
-    console.log('Voice Air Funk:', voiceAirFunk);
-    console.log('Custom Voice:', voiceCustomVoice);
-    console.log('Voice Players:', voicePlayers);
-    */ 
-
-    res.send(`<script> ${codeToExecute} </script>`);
+    res.send(`<script>${codeToExecute}</script>`);
 });
